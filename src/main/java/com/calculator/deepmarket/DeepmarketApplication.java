278944d571fc083bed40e5b36f5b464682d2e7b7
Esau Torres <davidesau140@gmail.com>
2025-07-28 19:54:54 -0600

    my firts commit

---
+++ src/main/java/com/calculator/deepmarket/DeepmarketApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.deepmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calculator/deepmarket/DeepmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.deepmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
